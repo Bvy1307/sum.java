b85440f1f7a88dc11b7f630abdd57722494657e3
Bvy1307 <117834105+Bvy1307@users.noreply.github.com>
2023-03-24 09:19:42 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -4,5 +4,9 @@ public class sum {
         int b = 78;
         int c = a+b;
         System.out.println(a+" and "+b +" sum of: "+c );
+        
+         // other type to solve this problem, so,you can easily choosen any one of then and written you programs.
+        System.out.format("%d + %d = %d", +a,+b,+c ); //type-1
+        System.out.printf("\n%d + %d = %d",+a,+b,+c); //type-2
     }
 }
