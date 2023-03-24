198c3875e65799ebc5b3536bdbfc052654846aaf
Bvy1307 <117834105+Bvy1307@users.noreply.github.com>
2023-03-24 08:24:10 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 78;
+        int c = a+b;
+        System.out.println(a+" and "+b +" sum of: "+c );
+    }
+}
